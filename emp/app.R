92a1672adb71b6d32fb3d79d2a24e3c842d63c03
Zihan <zihan9701@gmail.com>
2020-12-02 22:04:03 -0500

    Update app.R

--- emp/app.R
+++ emp/app.R
@@ -1,50 +1,26 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(tidyr)
+library(DT)
+library(PythonInR)
+library(pdftools)
+library(rsconnect)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+
+
+pyConnect()
+pyExecfile("helper.py")
+
+ui = 
+  dashboardPage(
+    dashboardHeader(title = 'JobScanner'),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("About", tabName = "about", icon = icon("info")),
+        menuItem("Search Job", tabName = "search", icon = icon("database")),
+        fileInput('file1', "Insert your JD as PDF file",
+                  accept=c('pdf'))
+        
       )
-   )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
 
