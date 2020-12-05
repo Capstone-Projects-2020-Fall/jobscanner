a9c8f236428a5d4d1aa73e828a96ace64f71557f
Kevin Levitz <tug97195@temple.edu>
2020-12-04 22:35:11 -0500

    update 2nd panel
    update 2nd panel

--- app2/global.R
+++ app2/global.R
@@ -5,9 +5,19 @@ library(dplyr)
 library(tm)
 library(wordcloud)
 library(memoise)
-
+library(corrplot)
+library(stringr)
 
 
 #read data set
 data = read.csv('review_data.csv',stringsAsFactors=FALSE)
 
+#input option:
+choice = c('overall.ratings', 
+           'work.balance.stars',
+           'culture.values.stars',
+           'carrer.opportunities.stars',
+           'comp.benefit.stars',
+           'senior.mangemnet.stars')
+
+companies = c('google','amazon','facebook','netflix','apple','microsoft')
