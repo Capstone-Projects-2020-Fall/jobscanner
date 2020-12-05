4a9a146e633552dfc83245d60ff8950f39f06292
Chizhang <54897894+Chizhang2019@users.noreply.github.com>
2020-12-04 23:07:51 -0500

    python function

---
+++ userPanel/helper.py
@@ -0,0 +1,26 @@
+def get_best_match(myCV):
+    import numpy as np
+    import pandas as pd
+
+    gds = pd.read_json('https://www.dropbox.com/s/xy8u3txosmnhzim/gd.json?dl=1',
+                       orient='records')
+
+    def Jaccard(x, y):
+        """returns the jaccard similarity between two lists """
+        intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
+        union_cardinality = len(set.union(*[set(x), set(y)]))
+        return intersection_cardinality / float(union_cardinality)
+
+    def cal_similarity(cv):
+        similarity = []
+        for skills in gds.Job_Description:
+            similarity.append(Jaccard(cv, skills))
+
+        gds['similarity'] = similarity
+        col = ['Job_Title', 'Company_Name', 'Location', 'similarity']
+        best = gds.sort_values(by='similarity', ascending=False).head(100).loc[:, col]
+        return(best)
+
+    BestMatch = cal_similarity(myCV)
+
+    return BestMatch.to_csv('BestMatch.csv', encoding='utf-8')
