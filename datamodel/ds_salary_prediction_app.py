6b87e5611e3bc21f4364b0c9830966a8f2def08a
Ruchit <54867567+Ruchit42@users.noreply.github.com>
2020-12-06 09:49:02 -0500

    website title attached

--- datamodel/ds_salary_prediction_app.py
+++ datamodel/ds_salary_prediction_app.py
@@ -6,4 +6,14 @@ import time
 
 df = pd.read_csv("EDA_dp.csv")
 
+# st.write(df.head())
+
+st.title('Computer Science Salary Predictor Application')
+st.write("""
+
+This app predicts the Salary for **computer science graduates** over 2 years based on Machine Learning Algorithms (linear regression model and decision tree), also the job salary model scrapped by job scanner team for CIS 4398.
+""")
+
+st.subheader('Company Details \n Check Glassdoor for exact values, if unsure')
+
 
