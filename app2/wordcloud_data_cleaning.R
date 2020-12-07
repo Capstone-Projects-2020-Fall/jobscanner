151be0b4abd57f19e0c436ee7465d0b7152556f8
Kevin Levitz <tug97195@temple.edu>
2020-12-07 01:58:01 -0500

    Update wordcloud_data_cleaning.R

--- app2/wordcloud_data_cleaning.R
+++ app2/wordcloud_data_cleaning.R
@@ -12,3 +12,26 @@ google_summary =
 corpus_google_summary =
   Corpus(VectorSource(google_summary$summary))
 
+corpus_google_summary[[1]][1]
+
+# Text Cleaning
+
+#convert the text to lower case
+corpus_google_summary = 
+  tm_map(corpus_google_summary, content_transformer(tolower))
+#remove numbers
+corpus_google_summary = 
+  tm_map(corpus_google_summary, removeNumbers)
+#remove english commom stopwords
+corpus_google_summary = 
+  tm_map(corpus_google_summary, removeWords,stopwords("english"))
+# remove punctuations
+corpus_google_summary = 
+  tm_map(corpus_google_summary, removePunctuation)
+# eliminate extra white spaces
+corpus_google_summary = 
+  tm_map(corpus_google_summary, stripWhitespace)
+#Stemming Texts
+#corpus_google_summary = 
+#  tm_map(corpus_google_summary, stemDocument)
+
